2c4ef023d9cabec90156d97c764390c0074339ed
Juani <juan.rodriguezw@uniacc.edu>
2023-04-03 14:30:10 -0400

    Parche 0.7 xd no se que parche sería este
    Optimización cajas
    Bug TP fix
    Canvas Res
    Puntaje

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public int puntos = 0;
+    public TMP_Text puntajeText;
+    
     
     public void sumarPuntos(int puntosGanados)
     {
         puntos += puntosGanados;
+       
+       
         Debug.Log("puntos: " + puntos);
+    }
+
+    private void Update()
+    {
+        puntajeText.text = "Puntaje: " + puntos;
     }
 }
